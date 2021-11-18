c27b00551fd4d5d12ff12ed78a74b8d9e9727feb
li-truong <tan.ngoc.13112000@gmail.com>
2021-11-19 00:32:12 +0700

    Fix Detail Mark Teacher

--- src/screens/ClassScreen/ClassScreenTeacher/components/DetailSubmitTeacher.js
+++ src/screens/ClassScreen/ClassScreenTeacher/components/DetailSubmitTeacher.js
@@ -1,6 +1,6 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Alert, TouchableOpacity, Modal, TextInput } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons'; 
+import { MaterialIcons } from '@expo/vector-icons';
 
 import CustomButton from "../../../../components/CustomButton"
 
@@ -15,8 +15,8 @@ const DATA = [
     mark: '8/10',
     contentSubmit: 'Đây là bài làm của Nguyễn Văn B'
   },
-  
-  
+
+
 ];
 // const Item = ({ fullName, mark, timeSubmit }) => (
 //     <View style={styles.container}>
@@ -24,7 +24,7 @@ const DATA = [
 //             <Text style={styles.nameText}>{fullName}</Text>
 //             <Text style={styles.markText}>Điểm: {mark}</Text>
 //             <Text style={styles.timeSubmitText}>Ngày nộp: {timeSubmit}</Text>
-            
+
 //         </View>
 //         <View style={styles.listResult}>
 //             <CustomButton title={'Chi tiết'} />
@@ -32,7 +32,7 @@ const DATA = [
 //     </View>
 // );
 
-const DetailSubmitTeacher = ({route, item}) => {
+const DetailSubmitTeacher = ({ route, item }) => {
 
   const [modalVisible, setModalVisible] = useState(false);
   const [text, onChangeText] = React.useState(null);
@@ -47,21 +47,21 @@ const DetailSubmitTeacher = ({route, item}) => {
         {/* <View style={styles.submitContent}>
           <Text style={{}}> Bài làm</Text>
         </View> */}
-        <Text style={{textAlign:'center', fontSize: 14, marginTop:10,}}>Bài làm</Text>
+        <Text style={{ textAlign: 'center', fontSize: 14, marginTop: 10, }}>Bài làm</Text>
         {/* <Text style={{fontSize: 14, marginTop:10}}>Đây là bài làm của Nguyễn Văn Aaaaaaaaaaaaaaaaaaaaaaaaa</Text> */}
         <View>
-          <Text style={{textAlign:'center', fontSize: 14, marginTop:10, marginBottom: 250}}>Đây là bài làm của Nguyễn Văn A</Text>
+          <Text style={{ textAlign: 'center', fontSize: 14, marginTop: 10, marginBottom: 300 }}>Đây là bài làm của Nguyễn Văn A</Text>
         </View>
         {/* <TouchableOpacity style={styles.buttonSubmit} onPress={() => Alert.alert('Đã thêm học sinh')}>
             <Text style={styles.buttonSubmitText}>Phản hồi</Text>
           </TouchableOpacity> */}
         <View>
-          <TouchableOpacity style={styles.buttonSubmit} onPress={() => setModalVisible(true)} >
-            <Text style={styles.buttonSubmitText}>Phản hồi</Text>
+          <TouchableOpacity style={styles.buttonFeedback} onPress={() => setModalVisible(true)} >
+            <Text style={styles.buttonFeedbackText}>Phản hồi</Text>
           </TouchableOpacity>
         </View>
       </View>
-      {/* <Modal
+      <Modal
         animationType="slide"
         transparent={true}
         visible={modalVisible}
@@ -89,15 +89,15 @@ const DetailSubmitTeacher = ({route, item}) => {
             </View>
           </View>
         </View>
-      </Modal> */}
+      </Modal>
     </SafeAreaView>
   );
 }
 
 const TEXT = {
-    fontSize: 14,
-    color: '#000',
-    fontFamily: 'Roboto',
+  fontSize: 14,
+  color: '#000',
+  fontFamily: 'Roboto',
 }
 const TEXTBUTTON = {
   fontSize: 14,
@@ -106,140 +106,140 @@ const TEXTBUTTON = {
   fontWeight: 'bold'
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        margin: 10,
-        padding: 10,
-        borderColor: 'grey',
-        borderRadius: 5,
-        elevation: 3,
-        shadowColor: '#000000',
-        shadowOffset: {
-            width: 0,
-            height: 3,
-        },
-        shadowRadius: 5,
-        shadowOpacity: 1.0
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    margin: 10,
+    padding: 10,
+    borderColor: 'grey',
+    borderRadius: 5,
+    elevation: 3,
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
     },
-    nameMark: {
-        flex: 1,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-    },
-    nameText: {
-      flex:1,
-      ...TEXT,
-      fontSize: 20,
-      color:'#00A9B7',
-      fontWeight: 'bold',
-      
-    },
-    markText: {
-      flex:1,
-      ...TEXT,
-      fontSize:18,
-      marginLeft: 40,
-    },
-    contentSubmit:{
-      flex:15,
-    },
-    buttonSubmit:{
-      //flex:1,
-      backgroundColor: '#00A9B7',
-      height: 40,
-      alignItems:'center',
-      justifyContent: 'center',
-      borderRadius: 5,
-      marginRight: 5,
-      
+    shadowRadius: 5,
+    shadowOpacity: 1.0
   },
-  buttonSubmitText:{
+  nameMark: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  nameText: {
+    flex: 1,
+    ...TEXT,
+    fontSize: 20,
+    color: '#00A9B7',
+    fontWeight: 'bold',
+
+  },
+  markText: {
+    flex: 1,
+    ...TEXT,
+    fontSize: 18,
+    marginLeft: 40,
+  },
+  contentSubmit: {
+    flex: 15,
+  },
+  buttonFeedback: {
+    //flex:1,
+    backgroundColor: '#00A9B7',
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    marginRight: 5,
+
+  },
+  buttonFeedbackText: {
     ...TEXTBUTTON,
     elevation: 2,
-},
-centeredView: {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: '#000000AA',
-},
-modalView: {
-  borderRadius: 5,
-  alignItems: "center",
-  shadowColor: "#000",
-  shadowOffset: {
-    width: 0,
-    height: 2
   },
-  shadowOpacity: 0.25,
-  shadowRadius: 4,
-  elevation: 5,
-  justifyContent: 'center',
-  backgroundColor: 'white',
-  width: '90%',
-  height: 200,
-  borderWidth:1,
-  borderColor: '#B5B5B5',
-  padding: 10
-},
-contentAddClass: {
-  flex:1,
-  borderColor: '#B5B5B5',
-  borderRadius: 5,
-  justifyContent: 'space-around',
-  alignItems: 'center',
-},
-inputContainer: {
-  flex:1,
-  flexDirection: 'row',
-  backgroundColor: 'white',
-  borderWidth: 1,
-  borderColor: '#B5B5B5',
-  borderRadius: 5,
-  padding: 5,
-  marginVertical: 5,
-},
-inputClass: {
-  flex:1,
-},
-contentButton: {
-  flex:1,
-  flexDirection: 'row',
-  alignItems: "center",
-  justifyContent: 'space-around',
-  marginVertical: 5,
-},
-buttonSubmit:{
-  flex:1,
-  backgroundColor: '#00A9B7',
-  height: 40,
-  alignItems:'center',
-  justifyContent: 'center',
-  borderRadius: 5,
-  marginRight: 5,   
-},
-buttonCancel:{
-  flex:1,
-  backgroundColor: '#B5B5B5',
-  height: 40,
-  alignItems:'center',
-  justifyContent: 'center',
-  borderRadius: 5,
-  marginLeft: 5,
-},
-buttonSubmitText:{
-  ...TEXTBUTTON,
-  elevation: 2,
-},
-buttonCancelText: {
-  ...TEXTBUTTON,
-  elevation: 2,
-},
-    
-    
-    
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000AA',
+  },
+  modalView: {
+    borderRadius: 5,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    width: '90%',
+    height: 200,
+    borderWidth: 1,
+    borderColor: '#B5B5B5',
+    padding: 10
+  },
+  contentAddClass: {
+    flex: 1,
+    borderColor: '#B5B5B5',
+    borderRadius: 5,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: '#B5B5B5',
+    borderRadius: 5,
+    padding: 5,
+    marginVertical: 5,
+  },
+  inputClass: {
+    flex: 1,
+  },
+  contentButton: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: 'space-around',
+    marginVertical: 5,
+  },
+  buttonSubmit: {
+    flex: 1,
+    backgroundColor: '#00A9B7',
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    marginRight: 5,
+  },
+  buttonCancel: {
+    flex: 1,
+    backgroundColor: '#B5B5B5',
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    marginLeft: 5,
+  },
+  buttonSubmitText: {
+    ...TEXTBUTTON,
+    elevation: 2,
+  },
+  buttonCancelText: {
+    ...TEXTBUTTON,
+    elevation: 2,
+  },
+
+
+
 
 });
 
