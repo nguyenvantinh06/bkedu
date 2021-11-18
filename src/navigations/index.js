f191ad6ba45acaac098220f7cd0c14f96f6fe5ac
skylie628 <37264206+skylie628@users.noreply.github.com>
2021-11-19 00:56:37 +0700

    Student role

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,4 +1,21 @@
-const Navigations = {
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginStackNavigator from './LoginStackNavigator';
+import { BottomTabNavigatorTeacher } from "./TabNavigator";
 
+const Stack = createNativeStackNavigator();
+
+function AppNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="LoginStackNavigator" component={LoginStackNavigator}
+        options={{ headerShown: false }} />
+      <Stack.Screen name="BottomTabNavigatorTeacher" component={BottomTabNavigatorTeacher}
+        options={{ headerShown: false }} />
+      <Stack.Screen name="BottomTabNavigatorStudent" component={BottomTabNavigatorStudent}
+              options={{ headerShown: false }} />
+    </Stack.Navigator>
+  );
 }
-export default Navigations;
+
+export default AppNavigator;
